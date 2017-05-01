b93e0fe166bd0b6bef29d45c9b0916d3b11bcaf6
mikemiksch <mikemiksch@gmail.com>
2017-05-01 10:46:52 -0700

    Initial Commit

---
+++ location-reminders/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  location-reminders
+//
+//  Created by Mike Miksch on 5/1/17.
+//  Copyright © 2017 Mike Miksch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ location-reminders/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  location-reminders
+//
+//  Created by Mike Miksch on 5/1/17.
+//  Copyright © 2017 Mike Miksch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
